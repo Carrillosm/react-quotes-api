c423ef3e59a04c971af8ac13a93b7c5c5dd0d00b
Carrillosm <93622955+Carrillosm@users.noreply.github.com>
2023-05-16 14:49:24 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Quotes from "./Quotes";
+import { useState, useEffect } from "react";
+import css from "./App";
+function App() {
+  return (
+    <div className="App">
+    <Quotes></Quotes>
+    </div>
+  );
+}
+
+
+    
+export default App;
